69531432550f1d9c0f603e5eea60138787360b3f
pratik-londhe4 <pratik.londhe@moderncoe.edu.in>
2022-02-09 09:42:20 +0530

    added redirect to tasks for /

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,17 @@
 
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
+
+def indexRedirect(req):
+    return HttpResponseRedirect("tasks/")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('user/' , include("user.urls")),
-    path('tasks/' , include("tasks.urls"))
+    path('tasks/' , include("tasks.urls")),
+    path('' , indexRedirect)
 
 ]
+
